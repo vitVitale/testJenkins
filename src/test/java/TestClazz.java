31665de0a52017cd4dbdcafc3e66bc208d60e217
Vitaliy V <vit-vasilyuk@yandex.ru>
2019-02-28 17:43:13 +0300

    Initial commit

---
+++ src/test/java/TestClazz.java
@@ -0,0 +1,48 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by burakergoren
+ * class JenkinsDemo
+ */
+
+public class TestClazz {
+
+
+    private static String Base_Url = "https://www.facebook.com";
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        //System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(Base_Url);
+    }
+
+    @After
+    public void after() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCasePassed() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login_form']")).isDisplayed());
+    }
+
+    @Test
+    public void testCaseFailed() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
+    }
+
+    @Ignore
+    @Test
+    public void testCaseIgnored() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
+    }
+}
